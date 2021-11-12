01dd89539649b56eca6e4835b625e636a5227335
Sofia Wawrzyniak <fiawawrz@sas.upenn.edu>
2021-11-11 20:53:15 -0500

    Updates to user schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,18 +4,20 @@ const { Schema } = mongoose;
 
 interface IUser extends mongoose.Document {
   _id: string;
-  firstName: string;
-  lastName: string;
+  first_name: string;
+  last_name: string;
   email: string;
   password: string;
+  type: string;
   refreshToken: string;
 }
 
 const UserSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  type: { type: String, required: true },
   refreshToken: { type: String, required: false },
 });
 
