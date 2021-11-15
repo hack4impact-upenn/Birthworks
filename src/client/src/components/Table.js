42852174238a2c7c87564aa6306746a35c080081
Silvi Kabra <silvi.kabra@gmail.com>
2021-11-15 01:14:12 -0500

    abstracted table and made customer table and page

---
+++ src/client/src/components/Table.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import styled from 'styled-components';
+import Colors from '../common/Colors';
+
+const StyledTable = styled.table`
+  border: 1px solid #000000;
+  display: table;
+  width: 100%;
+  padding-bottom: 20px;
+  border-collapse: separate;
+  border-spacing: 0px 10px;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  th {
+    font-weight: 600;
+    font-size: 16px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid #000000;
+    align-items: center;
+    text-align: center;
+    color: #000000;
+  }
+  th#col0 {
+    padding-left: 1%;
+  }
+  th#col1 {
+    width: 20%;
+    padding-left: 30px;
+  }
+  th#col2 {
+    width: 60%;
+    text-align: left;
+    align-items: left;
+    padding-left: 80px;
+  }
+  th#col3 {
+    text-align: left;
+    align-items: left;
+    padding-right: 20px;
+  }
+  td {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid #969696;
+  }
+  td#dataCol0 {
+    position: relative;
+    top: 4px;
+    padding-left: 4%;
+  }
+  td#dataCol1 {
+    width: 20%;
+    position: relative;
+    top: 4px;
+    padding-left: 7%;
+  }
+  td#dataCol2 {
+    width: 60%;
+    position: relative;
+    top: 4px;
+    text-align: left;
+    padding-left: 80px;
+  }
+  td#dataCol3 {
+    position: relative;
+    top: 4px;
+    padding-right: 20px;
+    color: #ff0505;
+    font-weight: 600;
+  }
+`;
+
+function getDeleteElement(i, j) {
+  console.log('delete col');
+  console.log(`dataCol${j}`);
+  return (
+    <td key={`row${i}col${j}`} id={`dataCol${j}`}>
+      Delete
+    </td>
+  );
+}
+
+function Table(props) {
+  return (
+    <StyledTable>
+      <thead>
+        <tr>
+          {props.headerColumns.map(function (col, i) {
+            return (
+              <th key={`col${i}`} id={`col${i}`}>
+                {col}
+              </th>
+            );
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map(function (entry, i) {
+          if (props.hoverable) {
+            return (
+              <tr key={i} onClick={props.rowLink}>
+                {props.dataColumns.map(function (col, j) {
+                  if (col == 'Delete') {
+                    return getDeleteElement(i, j);
+                  }
+                  return (
+                    <td key={`row${i}col${j}`} id={`dataCol${j}`}>
+                      {entry[col]}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          } else {
+            return (
+              <tr key={i}>
+                {props.dataColumns.map(function (col, j) {
+                  if (col == 'Delete') {
+                    return getDeleteElement(i, j);
+                  }
+                  return (
+                    <td key={`row${i}col${j}`} id={`dataCol${j}`}>
+                      {entry[col]}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          }
+        })}
+      </tbody>
+    </StyledTable>
+  );
+}
+
+export default Table;
--- src/client/src/pages/AdminUserManagementPage.js
+++ src/client/src/pages/AdminUserManagementPage.js
@@ -1,13 +1,40 @@
 import { Form } from 'formik';
 import React from 'react';
 import SearchBar from '../components/SearchBar';
+import Table from '../components/Table';
 import UserTable from '../components/UserTable';
+
+const placeholderUsers = [
+  {
+    Id: '1',
+    UserType: 'Admin',
+    Name: 'Cristin Tighe',
+    Email: 'programdirector@birthworks.org',
+  },
+  {
+    Id: '2',
+    UserType: 'User',
+    Name: 'Ziya Xu',
+    Email: 'ziyaxu@hack4impact.org',
+  },
+  {
+    Id: '3',
+    UserType: 'User',
+    Name: 'Mohamed Abaker',
+    Email: 'mohamed.abaker@hack4impact.org',
+  },
+];
 
 function AdminUserManagementPage() {
   return (
     <div className="container">
       <SearchBar></SearchBar>
-      <UserTable></UserTable>
+      <Table
+        headerColumns={['User Type', 'Name', 'Email', 'Delete']}
+        dataColumns={['UserType', 'Name', 'Email', 'Delete']}
+        data={placeholderUsers}
+        hoverable={false}
+      ></Table>
     </div>
   );
 }
