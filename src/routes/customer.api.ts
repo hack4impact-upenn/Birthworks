58e98f7aac5ea09c8ed09205d27834e96ee74939
Ben Demers <ben.n.demers@gmail.com>
2021-11-20 15:58:29 -0500

    stupid month math

--- src/routes/customer.api.ts
+++ src/routes/customer.api.ts
@@ -2,15 +2,38 @@ import { Customer, ICustomer } from '../models/customer.model';
 import express from 'express';
 import auth from '../middleware/auth';
 import errorHandler from './error';
+import mongoose from 'mongoose';
 
 const number_of_customers = 10;
 
 const router = express.Router();
 
+function getMaxExpDate(months: number) {
+  const dateObj = new Date();
+  let month = dateObj.getMonth() + 1 + months;
+  const day = 1;
+  let year = dateObj.getFullYear();
+  console.log(`${year}, ${month}`);
+  if (month > 11) {
+    year = year + Math.floor(month / 12);
+    month = month % 12;
+  }
+  return new Date(year, month, day);
+}
+
 router.get('/', async (req, res) => {
+  console.log(getMaxExpDate(27));
+  console.log(getMaxExpDate(1));
   const { filters } = req.body.filters;
   const { query } = req.body.query;
-  Customer.find({})
+  Customer.find({
+    $or: [
+      { first_name: { $regex: query } },
+      { last_name: { $regex: query } },
+      { email: { $regex: query } },
+      { phone: { $regex: query } },
+    ],
+  })
     .limit(number_of_customers)
     .then((result) => res.status(200).json({ success: true, result }))
     .catch((e) => errorHandler(res, e));
@@ -25,21 +48,23 @@ const patch_err_msg = 'Error updating customer notes';
 router.patch('/:id', async (req, res) => {
   const { id } = req.params;
   const { notes } = req.body;
-  const customer = await Customer.findByIdAndUpdate(
-    id,
-    { notes_write: notes },
-    (err, result) => {
-      if (err) {
-        return errorHandler(res, patch_err_msg);
-      }
-      return res.status(200).json({ success: true, result });
+  console.log(notes);
+  Customer.findByIdAndUpdate(id, { notes_write: notes }, (err, _) => {
+    if (err) {
+      return errorHandler(res, patch_err_msg);
     }
-  );
+    return res
+      .status(200)
+      .json({ success: true, result: 'Notes successfully updated' });
+  });
 });
 
 router.get('/:customer_id', async (req, res) => {
   const { customer_id } = req.params;
+  console.log(customer_id);
   Customer.findById(customer_id)
     .then((result) => res.status(200).json({ success: true, result }))
     .catch((e) => errorHandler(res, e));
 });
+
+export default router;
