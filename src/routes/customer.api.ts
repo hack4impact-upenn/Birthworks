99e2df230f96cdb9121471a57731e1098eb32db8
Ben Demers <ben.n.demers@gmail.com>
2021-12-01 09:52:07 -0500

    getting closer to certification filtering
    Co-authored-by: Aditya Bhati  <adisb01@users.noreply.github.com>

--- src/routes/customer.api.ts
+++ src/routes/customer.api.ts
@@ -1,8 +1,10 @@
 import { Customer, ICustomer } from '../models/customer.model';
+import { Cert, ICert } from '../models/cert.model';
 import express from 'express';
 import auth from '../middleware/auth';
 import errorHandler from './error';
 import mongoose from 'mongoose';
+import { result } from 'lodash';
 
 const number_of_customers = 10;
 
@@ -20,12 +22,45 @@ function getMaxExpDate(months: number) {
   }
   return new Date(year, month, day);
 }
+// TODO: see if we actually need to calculate and/or push the recert date?
+function expiresTooSoon(
+  months: number,
+  element: ICert,
+  index: number,
+  array: [ICert]
+) {
+  const exp_months = element.name === 'Kangaroula' ? 12 : 24;
+  const recertification_date = new Date(
+    element.completion_date.setMonth(
+      element.completion_date.getMonth() + exp_months
+    )
+  );
+  return recertification_date > getMaxExpDate(months);
+}
+
+function customerInvalid(c: any, filter: any) {
+  console.log(filter);
+  const certs = c.cert_obj;
+  let recertifications_bad = false;
+  if (filter.recertification != 'undefined' && filter != '') {
+    recertifications_bad = certs.some(
+      expiresTooSoon.bind(null, filter.recertification)
+    );
+  }
+  let membership_bad = false;
+  if (filter.membership != 'undefined' && filter != '') {
+    membership_bad = c.membership_end > getMaxExpDate(filter.membership);
+  }
+  return recertifications_bad || membership_bad;
+}
 
 router.post('/', async (req, res) => {
   console.log(getMaxExpDate(27));
   console.log(getMaxExpDate(1));
   const { page_num } = req.body;
   const { filters } = req.body;
+  console.log(req.body);
+  console.log(filters);
   const { query } = req.body;
   let find_query = {};
   if (typeof query !== 'undefined') {
@@ -38,9 +73,22 @@ router.post('/', async (req, res) => {
       ],
     };
   }
-  Customer.find(find_query)
+  Customer.aggregate([
+    {
+      $lookup: {
+        from: 'certifications',
+        localField: 'certifications',
+        foreignField: '_id',
+        as: 'cert_obj',
+      },
+    },
+  ])
+    .match(find_query)
     .skip(page_num * number_of_customers)
     .limit(number_of_customers)
+    .then((result) =>
+      result.filter((customer) => customerInvalid(customer, filters))
+    )
     .then((result) => res.status(200).json({ success: true, result }))
     .catch((e) => errorHandler(res, e));
 });
