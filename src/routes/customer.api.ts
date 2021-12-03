6a6e744ea5320d1d9ac8dc973eb691b6e5f5f4e9
Ben Demers <ben.n.demers@gmail.com>
2021-12-03 16:43:56 -0500

    ezpz task donezo
    Co-authored-by: Aditya Bhati  <adisb01@users.noreply.github.com>

--- src/routes/customer.api.ts
+++ src/routes/customer.api.ts
@@ -3,8 +3,6 @@ import { Cert, ICert } from '../models/cert.model';
 import express from 'express';
 import auth from '../middleware/auth';
 import errorHandler from './error';
-import mongoose from 'mongoose';
-import { result } from 'lodash';
 
 const number_of_customers = 10;
 
@@ -15,14 +13,20 @@ function getMaxExpDate(months: number) {
   let month = dateObj.getMonth() + 1 + months;
   const day = 1;
   let year = dateObj.getFullYear();
-  console.log(`${year}, ${month}`);
   if (month > 11) {
     year = year + Math.floor(month / 12);
     month = month % 12;
   }
   return new Date(year, month, day);
 }
-// TODO: see if we actually need to calculate and/or push the recert date?
+/**
+ * Checks to see if a certifcation expires within a given number of months.
+ * Calculated by looking at the latest recertification date of the certification
+ * or uses the original certification date if there's no recertifications.
+ * @param months The number of months to check for a potential expiration
+ * @param element The certification to check
+ * @returns A boolean saying if it expires within the range or not
+ */
 function expiresTooSoon(
   months: number,
   element: ICert,
@@ -30,17 +34,21 @@ function expiresTooSoon(
   array: [ICert]
 ) {
   const exp_months = element.name === 'Kangaroula' ? 12 : 24;
-  const recertification_date = new Date(
-    element.completion_date.setMonth(
-      element.completion_date.getMonth() + exp_months
-    )
+  const recert_dates = element.recertification_dates;
+  let latest_completion = element.completion_date;
+  if (recert_dates.length > 0) {
+    latest_completion = recert_dates[recert_dates.length - 1];
+  }
+  latest_completion = new Date(latest_completion);
+  const recertification_deadline = new Date(
+    latest_completion.setMonth(latest_completion.getMonth() + exp_months)
   );
-  return recertification_date > getMaxExpDate(months);
+  return recertification_deadline < getMaxExpDate(months);
 }
 
 function customerInvalid(c: any, filter: any) {
-  console.log(filter);
-  const certs = c.cert_obj;
+  const temp_c = JSON.parse(JSON.stringify(c));
+  const certs = temp_c.cert_obj;
   let recertifications_bad = false;
   if (filter.recertification != 'undefined' && filter != '') {
     recertifications_bad = certs.some(
@@ -49,27 +57,24 @@ function customerInvalid(c: any, filter: any) {
   }
   let membership_bad = false;
   if (filter.membership != 'undefined' && filter != '') {
-    membership_bad = c.membership_end > getMaxExpDate(filter.membership);
+    membership_bad = c.membership_end < getMaxExpDate(filter.membership);
   }
+  console.log('');
   return recertifications_bad || membership_bad;
 }
 
 router.post('/', async (req, res) => {
-  console.log(getMaxExpDate(27));
-  console.log(getMaxExpDate(1));
   const { page_num } = req.body;
   const { filters } = req.body;
-  console.log(req.body);
-  console.log(filters);
   const { query } = req.body;
   let find_query = {};
   if (typeof query !== 'undefined') {
     find_query = {
       $or: [
-        { first_name: { $regex: query } },
-        { last_name: { $regex: query } },
-        { email: { $regex: query } },
-        { phone: { $regex: query } },
+        { first_name: { $regex: query, $options: 'i' } },
+        { last_name: { $regex: query, $options: 'i' } },
+        { email: { $regex: query, $options: 'i' } },
+        { phone: { $regex: query, $options: 'i' } },
       ],
     };
   }
@@ -84,13 +89,13 @@ router.post('/', async (req, res) => {
     },
   ])
     .match(find_query)
-    .skip(page_num * number_of_customers)
+    .skip((page_num - 1) * number_of_customers)
     .limit(number_of_customers)
-    .then((result) =>
-      result.filter((customer) => customerInvalid(customer, filters))
-    )
-    .then((result) => res.status(200).json({ success: true, result }))
-    .catch((e) => errorHandler(res, e));
+    .then((result) => {
+      result = result.filter((customer) => customerInvalid(customer, filters));
+      res.status(200).json({ success: true, result });
+    })
+    .catch((e) => console.log(e));
 });
 
 /**
@@ -102,7 +107,6 @@ const patch_err_msg = 'Error updating customer notes';
 router.patch('/:id', async (req, res) => {
   const { id } = req.params;
   const { notes } = req.body;
-  console.log(notes);
   Customer.findByIdAndUpdate(id, { notes_write: notes }, (err, _) => {
     if (err) {
       return errorHandler(res, patch_err_msg);
@@ -115,7 +119,6 @@ router.patch('/:id', async (req, res) => {
 
 router.get('/:customer_id', async (req, res) => {
   const { customer_id } = req.params;
-  console.log(customer_id);
   Customer.findById(customer_id)
     .then((result) => res.status(200).json({ success: true, result }))
     .catch((e) => errorHandler(res, e));
