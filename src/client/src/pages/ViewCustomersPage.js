8775e34df25dd457b07a1973e07efde9923b8ba3
taran317 <67604374+taran317@users.noreply.github.com>
2021-12-04 15:10:59 -0500

    merged filter task and pagination task

--- src/client/src/pages/ViewCustomersPage.js
+++ src/client/src/pages/ViewCustomersPage.js
@@ -1,42 +1,6 @@
-import React from 'react';
 import Table from '../components/Table';
+import React, { useState } from 'react';
 import UserFilter from '../components/UserFilter';
-
-const certifications = [
-  {
-    entryDate: Date.now(),
-    completionDate: Date.now(),
-    recertificationDate: Date.now(),
-    certificationDate: Date.now(),
-    trainer: 'some trainer',
-    name: 'some name',
-  },
-  {
-    entryDate: Date.now(),
-    completionDate: Date.now(),
-    recertificationDate: Date.now(),
-    certificationDate: Date.now(),
-    trainer: 'cert trainer',
-    name: 'another name',
-  },
-];
-
-const workshops = [
-  {
-    startDate: Date.now(),
-    endDate: Date.now(),
-    location: 'virtual',
-    mentor: 'some trainer',
-    name: 'some name',
-  },
-  {
-    startDate: Date.now(),
-    endDate: Date.now(),
-    location: 'virtual',
-    mentor: 'some trainer',
-    name: 'some name',
-  },
-];
 
 const options1 = [
   {
@@ -88,6 +52,8 @@ const options3 = [
 
 const name3 = 'Program';
 
+const maxPageNumber = 3;
+
 const placeholderCustomers = [
   {
     Id: '1',
@@ -107,9 +73,147 @@ const placeholderCustomers = [
     Email: 'ziyaxu@hack4impact.org',
     PhoneNumber: 'XXX-XXX-XXXX',
   },
+  {
+    Id: '4',
+    Name: 'Ziya Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '5',
+    Name: 'Ziya Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '6',
+    Name: 'Ziya Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '7',
+    Name: 'Ziya Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '8',
+    Name: 'Ziya Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '9',
+    Name: 'Ziya Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '10',
+    Name: 'Ziya Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '11',
+    Name: 'Ziya "Page 2" Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '12',
+    Name: 'Ziya "Page 2" Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '13',
+    Name: 'Ziya "Page 2" Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '14',
+    Name: 'Ziya "Page 2" Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '15',
+    Name: 'Ziya "Page 2" Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '16',
+    Name: 'Ziya "Page 2" Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '17',
+    Name: 'Ziya "Page 2" Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '18',
+    Name: 'Ziya "Page 2" Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '19',
+    Name: 'Ziya "Page 2" Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '20',
+    Name: 'Ziya "Page 3" Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
+  {
+    Id: '21',
+    Name: 'Ziya "Page 3" Xu',
+    Email: 'ziyaxu@hack4impact.org',
+    PhoneNumber: 'XXX-XXX-XXXX',
+  },
 ];
 
 function ViewCustomersPage() {
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const increasePage = () => {
+    if (pageNumber < maxPageNumber) {
+      const newPageNumber = pageNumber + 1;
+      setPageNumber(newPageNumber);
+    }
+  };
+
+  const decreasePage = () => {
+    if (pageNumber > 1) {
+      const newPageNumber = pageNumber - 1;
+      setPageNumber(newPageNumber);
+    }
+  };
+
+  const goToEnd = () => {
+    const newPageNumber = maxPageNumber;
+    setPageNumber(newPageNumber);
+  };
+
+  const goToStart = () => {
+    const newPageNumber = 1;
+    setPageNumber(newPageNumber);
+  };
+
+  const getEntriesOnPage = (entry) => {
+    return Math.ceil(parseInt(entry.Id) / 10) == pageNumber;
+  };
+
   return (
     <div className="container">
       <UserFilter
@@ -123,10 +227,43 @@ function ViewCustomersPage() {
       <Table
         headerColumns={['Name', 'Email', 'Phone Number']}
         dataColumns={['Name', 'Email', 'PhoneNumber']}
-        data={placeholderCustomers}
+        data={placeholderCustomers.filter(getEntriesOnPage)}
         hoverable={true}
         rowLink={() => console.log('clicked')}
       ></Table>
+      <div>
+        <div class="columns is-mobile is-centered">
+          <div
+            onClick={goToStart}
+            style={{ cursor: 'pointer' }}
+            class="column is-narrow"
+          >
+            {'<<'}
+          </div>
+          <div
+            onClick={decreasePage}
+            style={{ cursor: 'pointer' }}
+            class="column is-narrow"
+          >
+            {'<'}
+          </div>
+          <div class="column is-narrow">{pageNumber}</div>
+          <div
+            onClick={increasePage}
+            style={{ cursor: 'pointer' }}
+            class="column is-narrow"
+          >
+            {'>'}
+          </div>
+          <div
+            onClick={goToEnd}
+            style={{ cursor: 'pointer' }}
+            class="column is-narrow"
+          >
+            {'>>'}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
